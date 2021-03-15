5240ec76f4c68ff9407e63e92f49526cb4223642
Alex Zharichenko <azharichenko@gmail.com>
2021-03-15 17:54:15 -0400

    Adding controller and display

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,5 +1,6 @@
 #include <FastLED.h>
 #define NUM_LEDS 300
+#define FPS 24
 
 CRGB leds[NUM_LEDS];
 
@@ -8,22 +9,13 @@ void setup()
   FastLED.addLeds<WS2812, 2, GRB>(leds, NUM_LEDS);
   FastLED.setMaxPowerInVoltsAndMilliamps(5, 400);
   Serial.begin(2152000);
-  Serial.setTimeout(5);
+  Serial.setTimeout(8);
 }
 
 void loop() {
   if (Serial.available()) {
-    if (Serial.readBytes( (char*)leds, NUM_LEDS * 3 + 1) != 0) {
-      //    for (int i = 0; i < NUM_LEDS; i++) {
-      //      Serial.print(leds[i].r);
-      //      Serial.print(',');
-      //      Serial.print(leds[i].g);
-      //      Serial.print(',');
-      //      Serial.print(leds[i].b);
-      //      Serial.print('\n');
-      //    }
+    if (Serial.readBytes( (char*)leds, NUM_LEDS * 3) != 0) {
       FastLED.show();
-
     }
   }
 }
